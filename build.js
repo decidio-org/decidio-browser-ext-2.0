f074eb4183d2f153d619ecff673d3a4644880986
Tahlia Kamieniecki <tahlia@kamieniecki.com>
2026-05-21 13:00:40 -0400

    complete directiry overhaul for cross-platform compatablity

---
+++ build.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+function build(browser) {
+    const distDir = path.join(__dirname, `dist-${browser}`);
+
+    // Creates the browser-specific target folder
+    if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
+
+    // Copy code assets into it from the src/ folder
+    fs.copyFileSync('src/background.js', path.join(distDir, 'background.js'));
+    fs.copyFileSync('src/overlay.html', path.join(distDir, 'overlay.html'));
+    fs.copyFileSync('src/content.js', path.join(distDir, 'content.js'));
+    fs.copyFileSync('src/overlay_helper.js', path.join(distDir, 'overlay_helper.js'));
+
+    // Update asset copying to pull from src/
+    fs.copyFileSync('src/decidio_logo.png', path.join(distDir, 'decidio_logo.png'));
+
+
+    // Copy data mock files (Ensure casing matches exactly)
+    fs.copyFileSync('sample.JSON', path.join(distDir, 'sample.json'));
+
+    // Rename and inject the correct manifest file from the root
+    fs.copyFileSync(`manifest.${browser}.json`, path.join(distDir, 'manifest.json'));
+
+    console.log(`✓ Built target for ${browser}`);
+}
+
+build('chrome');
+build('safari');
